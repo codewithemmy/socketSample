1933cd5ceae10863abf21e2f49d7d5f9b46fb0f3
codewithemmy <codewithemmy@gmail.com>
2023-07-31 09:19:49 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const { Server } = require("socket.io");
+const { createServer } = require("http");
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: { origin: "https://cityfix.onrender.com" },
+});
+
+// Define a route for your main application (optional)
+app.get("/", (req, res) => {
+  res.send("Hello, this is your Express app!");
+});
+
+// Socket.io event handling
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  // Event fired when a user disconnects
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+
+  // Example event: listen for a custom event 'chat message'
+  socket.on("chat message", (message) => {
+    console.log("Received message:", message);
+    // Broadcast the message to all connected clients
+    io.emit("chat message", message);
+  });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
